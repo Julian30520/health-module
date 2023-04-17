a33c466c8f059aacddfdd2eead947e067aae255c
Julian30520 <juliantomczyk@gmail.com>
2023-04-17 16:51:36 +0200

    Create get generic to return process state

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const childProcess = require('child_process');
 
 var app = express();
 
@@ -19,8 +20,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/*app.use('/', indexRouter);
+app.use('/users', usersRouter);*/
+
+app.get(`/api/process/:pname`, (req, res) => {
+  const processName = req.params.pname;
+
+  // Ajoutez le code pour vérifier le processus ici
+  childProcess.exec('tasklist', (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+    const isAlive = stdout.includes(processName);
+    const reponse = {
+      isAlive
+    };
+    res.json(reponse);
+  });
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
