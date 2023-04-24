311a4cfbead7e63dc8ccb859533a8a8ec2d54e51
Julian30520 <juliantomczyk@gmail.com>
2023-04-24 23:16:24 +0200

    Clear + install package

--- app.js
+++ app.js
@@ -1,27 +1,7 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
 const childProcess = require('child_process');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-/*app.use('/', indexRouter);
-app.use('/users', usersRouter);*/
+const app = express();
+const port = 3000;
 
 app.get(`/api/process/:pname`, (req, res) => {
   const processName = req.params.pname;
@@ -40,21 +20,6 @@ app.get(`/api/process/:pname`, (req, res) => {
   });
 });
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
